bce1e6500e6b5bffa587f085b0d111d057bd40cb
Vincent Washington <97264834+VWashingtonCoder@users.noreply.github.com>
2022-02-25 12:36:29 -0700

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,29 @@
 import React from 'react'
 // We'll need quite a few imports from react-router-dom
+import { 
+  NavLink, 
+  useParams, 
+  useRouteMatch, 
+  Route 
+} from 'react-router-dom';
 
 import ItemDetails from './ItemDetails'
 
 export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
-  const { items } = props
-
+  const { items } = props;
+  const { itemID } = useParams();
+  const { url, path } = useRouteMatch();
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
   // we use this hook to grab they dynamic parts of the path (:itemID).
-  const item = {}
+  if (!items.length) return "Hold yer horses, be back in a sec!";
 
+  const item = items.find(item => item.id === parseInt(itemID));
+  // NEVER EVER EVER USE DOUBLE EQUALS
+  // NEVER EVER EVER USE VAR
+  
   return (
     <div className='item-wrapper'>
       <div className='item-header'>
@@ -27,10 +38,18 @@ export default function Item(props) {
 
       <nav className='item-sub-nav'>
         {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
+        <NavLink to={`${url}/description`}>Description</NavLink>
+        <NavLink to={`${url}/shipping`}>Shipping</NavLink>
       </nav>
 
       {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
       {/* These Routes should render <ItemDetails /> */}
+      <Route path={`${path}/shipping`}>
+        <ItemDetails text={item.shipping} />
+      </Route>
+      <Route path={`${path}/description`}>
+        <ItemDetails text={item.description} />
+      </Route>
 
       {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
     </div>
